782cd1784f5694329d04380bde23fa889cd82c74
Valeri1234car <valerko.kamburov@gmail.com>
2026-06-05 10:42:08 +0200

    remove prisma connect on startup to prevent P1017 crash

--- api/src/plugins/prisma.js
+++ api/src/plugins/prisma.js
@@ -2,9 +2,16 @@ import fp from "fastify-plugin";
 import { PrismaClient } from "@prisma/client";
 
 async function prismaPlugin(app) {
-  const prisma = new PrismaClient();
-  await prisma.$connect();
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
   app.decorate("prisma", prisma);
+
   app.addHook("onClose", async () => {
     await prisma.$disconnect();
   });
